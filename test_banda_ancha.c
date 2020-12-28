faf3e6cd60e86a2a62bbaa8d9b73bf51402a1f51
cochoa_hiberus <cochoa@hiberus.com>
2020-12-28 21:34:36 +0100

    envio de paquietes test banda ancha uno a uno listo

--- test_banda_ancha.c
+++ test_banda_ancha.c
@@ -1,18 +1,40 @@
 #include<stdio.h>
+#include <stdlib.h>
 #include<mpi.h>
+#include <sys/time.h>
 
 int main( int argc, char* argv[] ) {
     int this_proc, total_procs;
 
     /* Aqui comienza el paralelismo */
     MPI_Init( &argc, &argv );
+    int np = atoi(argv[1]);
+    int psize = atoi(argv[2]);
+    int sendbuf[psize];
+    int recvbuf[psize];
+    int ack = 0;
+    struct timeval t1, t2;
     MPI_Comm_size( MPI_COMM_WORLD, &total_procs ); // number of process
     MPI_Comm_rank( MPI_COMM_WORLD, &this_proc );  //PID
     if( this_proc == 0){
-        printf("Even: %d", this_proc);
+        gettimeofday(&t1, NULL);
+        for (int i = 0; i < np; i++)
+        {
+            MPI_Send(&sendbuf, psize, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        }
+        MPI_Recv(&ack, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        gettimeofday(&t2, NULL);
+        int t_band = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
+        printf("-- Process [%d] sent %d bursts of size %d to process %d, burst time was %d microseconds.\n", this_proc, np, psize, this_proc + 1, t_band);
+        
     } else if (this_proc == 1)
     {
-        printf("Odd: % d", this_proc);
+        for (int i = 0; i < np; i++)
+        {
+            MPI_Recv(&recvbuf, psize, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        }
+        MPI_Send(&ack, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("Done!: Process %d.\n", this_proc);
     }
     
 
