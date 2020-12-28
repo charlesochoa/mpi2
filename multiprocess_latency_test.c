f0758e856de1942891d5b0c3c840ca7997743248
Gabriel Alvarez <gabrielaar11@gmail.com>
2020-12-28 19:37:06 +0100

    Add multiprocess latency test file

--- multiprocess_latency_test.c
+++ multiprocess_latency_test.c
@@ -4,52 +4,59 @@
 #include <mpi.h>
 #include <sys/time.h>
  
-int main( int argc, char* argv[] ) {
+int main(int argc, char* argv[]) {
     int this_proc;
     int task_id;
     int i;
-    int N; /* N is the number of pairs */
-    int M; /* M is the number of bounces */
+    int n; /* n is the number of pairs */
+    int m = -1; /* m is the number of bounces */
+    int p = -1;
     struct timeval t1, t2;
+ 
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &n);           // Number of processes
+    MPI_Comm_rank(MPI_COMM_WORLD, &this_proc);   // PID
 
-    if (argc == 5) {
-        i = 1;
-        while (i < 5) {
-            if (argv[i] == "-N") {
-                N = atoi(argv[i]);
-                i++;
-            } else if (argv[i] == "-M") {
-                M = atoi(argv[i]);
-                i++;
-            }
+    m = atoi(argv[1]);
+
+    while (i < argc) {
+        if (argv[i] == "-p") {
+            p = atoi(argv[i+1]);
+            i++;
+        } else if (argv[i] == "-m") {
+            m = atoi(argv[i+1]);
             i++;
         }
+        i++;
     }
-    else {
-        printf("Incorrect number of arguments supplied!\n");
+            
+    if ((m < 0) || (p < 0)) {
+        printf("Missing argument for number of bounces or size of packages!\n");
+        MPI_Finalize();
+        return 0;
     }
  
-    MPI_Init(NULL, NULL);
-    MPI_Comm_size(MPI_COMM_WORLD, &N); // Number of processes
-    MPI_Comm_rank(MPI_COMM_WORLD, &this_proc);   // PID
- 
-    printf("Hello word! I'm process number %d of %d processes.\n", this_proc, N);
+    printf("Hello word! I'm process number %d of %d processes.\n", this_proc, n);
 
     if (this_proc % 2 == 0) {
         task_id = this_proc;
-        gettimeofday(&t1, NULL);
-        MPI_Send(&task_id, 1, MPI_INT, this_proc + 1, 0, MPI_COMM_WORLD);
-        gettimeofday(&t2, NULL);
-        int t_send = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
-        printf("-- Process [%d] sent task id %d to process %d, send time was %d microseconds.\n", this_proc, task_id, this_proc + 1, t_send);
+        for (i = 0; i < m; i++) {
+            gettimeofday(&t1, NULL);
+            MPI_Send(&task_id, 1, MPI_INT, this_proc + 1, 0, MPI_COMM_WORLD);
+            gettimeofday(&t2, NULL);
+            int t_send = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
+            printf("-- [%d] Process [%d] sent task id %d to process %d, send time was %d microseconds.\n", i, this_proc, task_id, this_proc + 1, t_send);
+        }
     } else {
-        gettimeofday(&t1, NULL);
-        MPI_Recv(&task_id, 1, MPI_INT, this_proc - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        gettimeofday(&t2, NULL);
-        int t_recv = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
-        printf("-- Process [%d] received task id %d from process [%d], receive time was %d microseconds.\n", this_proc, task_id, this_proc - 1, t_recv);
+        for (i = 0; i < m; i++) {
+            gettimeofday(&t1, NULL);
+            MPI_Recv(&task_id, 1, MPI_INT, this_proc - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            gettimeofday(&t2, NULL);
+            int t_recv = (t2.tv_sec - t1.tv_sec) * 1000000 + t2.tv_usec - t1.tv_usec;
+            printf("-- [%d] Process [%d] received task id %d from process [%d], receive time was %d microseconds.\n", i, this_proc, task_id, this_proc - 1, t_recv);
+        }
     };
 
     MPI_Finalize();
-    return 0; 
+    return 0;
 }
