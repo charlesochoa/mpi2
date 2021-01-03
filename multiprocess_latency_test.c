8f6408930d8f634278730ae98266e4d2433bf9d4
Gabriel Álvarez <gabrielaar11@gmail.com>
2021-01-03 22:44:33 +0100

    Update multiprocess_latency_test.c

--- multiprocess_latency_test.c
+++ multiprocess_latency_test.c
@@ -51,7 +51,7 @@ int main(int argc, char* argv[]) {
             MPI_Send(sendbuffer, p, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
             end = MPI_Wtime();
             double t_send = end - start;
-            printf("-- Bounce number [%d] * Process [%d] sent packet of size %d bytes to process [%d], send time was %f seconds.\n", i, rank, p * size_of_int, rank - 1, t_send);
+            printf("-- Bounce number [%d] * Process [%d] sent packet of size %d bytes to process [%d], send time was %f seconds.\n", i, rank, p * size_of_int, rank + 1, t_send);
 
             // ********************** RECEIVE **********************
             start = MPI_Wtime();
@@ -67,7 +67,7 @@ int main(int argc, char* argv[]) {
             MPI_Recv(recvbuffer, p, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             end = MPI_Wtime();
             double t_recv = end - start;
-            printf("-- Bounce number [%d] * Process [%d] received packet of size %d bytes from process [%d], receive time was %f seconds.\n", i, rank, p * size_of_int, rank + 1, t_recv);
+            printf("-- Bounce number [%d] * Process [%d] received packet of size %d bytes from process [%d], receive time was %f seconds.\n", i, rank, p * size_of_int, rank - 1, t_recv);
 
             // ********************** SEND **********************
             start = MPI_Wtime();
