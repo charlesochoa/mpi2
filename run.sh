8c71df071e0a7a327559d7a6a41b85b500d21902
Gabriel Alvarez <gabrielaar11@gmail.com>
2021-01-06 22:22:36 +0100

    Measure bounce latency

--- run.sh
+++ run.sh
@@ -4,6 +4,13 @@ mpicc multiprocess_latency_test.c -o multiprocess_latency_test.o
 
 for i in 1 10 100
 do
+   echo "Running single pair multiprocess_latency_test..."
+   mpirun -n 2 --hostfile mihostfile.txt -mca btl_tcp_if_include br0 ./multiprocess_latency_test.o -m 100 -p $i >> single_pair_multiprocess_latency_test.csv 
+done
+
+for i in 1 10 100
+do
    echo "Running multiprocess_latency_test..."
    mpirun -n 8 --hostfile mihostfile.txt -mca btl_tcp_if_include br0 ./multiprocess_latency_test.o -m 100 -p $i >> multiprocess_latency_test.csv 
 done
+
